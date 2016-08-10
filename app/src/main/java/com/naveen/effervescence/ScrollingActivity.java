ee68e723c4a8b92f942f3c817836f410bcca45a4
Affan Ahmad Fahmi <affanahmadfahmi@gmail.com>
2016-08-10 22:25:10 +0530

    Changes made

--- app/src/main/java/com/naveen/effervescence/ScrollingActivity.java
+++ app/src/main/java/com/naveen/effervescence/ScrollingActivity.java
@@ -15,9 +15,7 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().hide();
+
         Typeface tf = Typeface.createFromAsset(getAssets(),"font.ttf");
         TextView textView = (TextView) findViewById(R.id.txt);
         textView.setTypeface(tf);
